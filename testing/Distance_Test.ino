459ccbda845364635a9382169fb71341f23bba3e
Curtis Mitchell <curtis@pop-os.localdomain>
2021-07-04 15:38:26 -0700

    distance test

---
+++ testing/Distance_Test.ino
@@ -0,0 +1,51 @@
+// testing the distance censors to check accuracy and how to interface with multiple of them
+
+// right distance sensor
+const int trigPinRight = 11;
+const int echoPinRight = 12;
+
+// left distance censor
+const int trigPinLeft = 5;
+const int echoPinLeft = 6;
+
+// initialize distance tracking variables
+float rightDistance = 0;
+float leftDistance = 0;
+
+void setup() {
+	// trig pin outputs pulses of electricity
+	// echo pin measures duration of pulses coming back
+	pinMode(trigPinRight, OUTPUT);
+	pinMode(echoPinRight, INPUT);
+	pinMode(trigPinLeft, OUTPUT);
+	pinMode(echoPinLeft, INPUT);
+
+	// change to 9600 if using Arduino IDE
+	// 115200 is for VS Code
+	Serial.begin(115200);
+}
+
+void loop() {
+	rightDistance = getDistance(trigPinRight, echoPinRight);
+	leftDistance = getDistance(trigPinLeft, echoPinLeft);
+
+	Serial.println("R: " + String(rightDistance) + " L: " + String(leftDistance));
+}
+
+float getDistance(const int trigPin, const int echoPin) {
+	// variable to store the time it takes for a ping to bounce off an object
+	float echoTime;
+	float calculatedDistance;
+
+	// send out an ultrasonic pulse that's 10ms long
+	digitalWrite(trigPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(trigPin, LOW);
+
+	// check how long it takes for the pulse to come back to the sensor
+	echoTime = pulseIn(echoPin, HIGH);
+	// calculate the distance of the object that reflected the pulse
+	calculatedDistance = echoTime / 148.0;
+
+	return calculatedDistance;
+}
